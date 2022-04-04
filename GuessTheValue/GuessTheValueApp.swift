75088101bce04cf804888416873444b57b5ced89
Alexey <alexeyradomskiy98@gmail.com>
2022-04-04 19:04:47 +0300

    Initial Commit

---
+++ GuessTheValue/GuessTheValueApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheValueApp.swift
+//  GuessTheValue
+//
+//  Created by Alexey on 04.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheValueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
